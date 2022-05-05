7d476e780817896303a9012be5691e48d9f76199
Em <ekramm@me.com>
2022-05-05 14:36:27 -0400

    commit tree c0e514015d914e3eb875a41bb6925076e4679303

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // Add your code here
+
+
+function submitData(name, email) {
+    const formData = {
+        name,
+        email,
+    };
+
+    const configurationObject = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify(formData),
+    }
+    return fetch('http://localhost:3000/users', configurationObject)
+        .then(response =>response.json())
+        .then(data => {
+            document.body.textContent = data.id;
+        })
+
+        .catch(error => {
+            alert('Bad things! failed to execute!');
+            document.body.textContent = error.message;
+        })
+
+}
+ 
